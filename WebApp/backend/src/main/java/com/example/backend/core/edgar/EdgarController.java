3fbe41c5f64b8c34b091e5c5c5a136848995ae1b
CodeIsMySpotter <codeismyspotter@gmail.com>
2026-07-30 22:09:58 +0200

    Clean unused imports

--- WebApp/backend/src/main/java/com/example/backend/core/edgar/EdgarController.java
+++ WebApp/backend/src/main/java/com/example/backend/core/edgar/EdgarController.java
@@ -1,7 +1,6 @@
 package com.example.backend.core.edgar;
 
 import java.util.List;
-import java.util.zip.DataFormatException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- WebApp/backend/src/main/java/com/example/backend/core/edgar/services/EdgarFetchServcie.java
+++ WebApp/backend/src/main/java/com/example/backend/core/edgar/services/EdgarFetchServcie.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
